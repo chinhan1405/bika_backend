53ce9dc9675b8ab1ac1c978c13f91b9e8c21888d
NhanHoang <nhan.hoang@saritasa.com>
2025-06-04 03:48:16 +0000

    Fix create assignment API

--- apps/assignment/api/views/assignment.py
+++ apps/assignment/api/views/assignment.py
@@ -51,7 +51,7 @@ class AssignmentViewSet(
         serializer.validated_data["creator"] = request.user
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return self.response(
+        return response.Response(
             serializer.data,
             status=status.HTTP_201_CREATED,
             headers=headers,
